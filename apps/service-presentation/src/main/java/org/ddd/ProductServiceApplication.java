31f8add8b4c6cb2e5c6d02b1e448dfe2cba53aa1
ivan.betanzos <ivan.betanzos89@gmail.com>
2023-12-11 12:33:08 +0100

    feat eureka server

--- apps/service-presentation/src/main/java/org/ddd/ProductServiceApplication.java
+++ apps/service-presentation/src/main/java/org/ddd/ProductServiceApplication.java
@@ -3,8 +3,10 @@ package org.ddd;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+@EnableDiscoveryClient
 @EnableJpaRepositories(basePackages = { "org.ddd.product", "org.ddd.category" })
 @EntityScan(basePackages = { "org.ddd.product", "org.ddd.category" })
 @SpringBootApplication(scanBasePackages = { "org.ddd.product", "org.ddd.category", "org.ddd.shared" })
